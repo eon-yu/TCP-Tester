72ca8b096e5385ab9848961fb6a8291898eb4795
eon-yu <eon.yu@stclab.com>
2025-08-06 17:37:20 +0900

    Handle out-of-bounds access in packet view

--- backend/models/tcp_server.go
+++ backend/models/tcp_server.go
@@ -1,15 +1,20 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 // TCPServer는 TCP 서버 연결 정보를 저장하는 모델입니다.
 type TCPServer struct {
-	gorm.Model
-	Name string `json:"name" gorm:"uniqueIndex"`
-	Host string `json:"host"`
-	Port int    `json:"port"`
+	ID        uint           `json:"id" gorm:"primaryKey"`
+	Name      string         `json:"name" gorm:"uniqueIndex"`
+	Host      string         `json:"host"`
+	Port      int            `json:"port"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`
 }
 
 // TCPServerRequest는 TCP 서버 생성/수정 요청 구조체입니다.
